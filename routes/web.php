ee22e9583a340f53efe0a3eab5f27fa72959ad31
reda193 <mohamed.reda33@outlook.com>
2025-04-06 13:36:48 -0400

    Started routes layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsletterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/signup', [NewsletterController::class, 'showSignupForm']);
+Route::post('/signup', [NewsletterController::class, 'processSignup']);
+Route::get('/success', [NewsletterController::class, 'showSuccess']);
+Route::get('/reload-captcha', [NewsletterController::class, 'reloadCaptcha']);
+Route::get('/unsubscribe/{email}', [NewsletterController::class, 'unsubscribe']);
